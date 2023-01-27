833cb1ac751f1e2ff9afea944a15fbd81fddaa78
Michael Wu <mw10013@gmail.com>
2023-01-27 13:17:50 -0500

    wip

--- apps/storefront-kit-tailwindui-demo/app/routes/_layout.products.tsx
+++ apps/storefront-kit-tailwindui-demo/app/routes/_layout.products.tsx
@@ -1,10 +1,13 @@
 import { Container } from "@/components/Container";
 import { graphql } from "@/lib/gql";
+import { ProductsQuery } from "@/lib/gql/graphql";
 import { shopClient } from "@/lib/utils";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import request from "graphql-request";
+
+const FIRST = 6;
 
 const query = graphql(`
   query Products($first: Int!, $after: String) {
@@ -32,7 +35,7 @@ export const loader = (async () => {
     document: query,
     requestHeaders: shopClient.getPublicTokenHeaders(),
     variables: {
-      first: 6,
+      first: FIRST,
       after: null,
     },
   });
@@ -42,16 +45,47 @@ export const loader = (async () => {
   });
 }) satisfies LoaderFunction;
 
-export default function Products() {
+type T = ProductsQuery;
+
+function ProductGrid() {
   // https://github.com/remix-run/remix/issues/5211
   const { data_ } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-y-0 sm:gap-x-6 lg:gap-x-8">
+      {data_.products.nodes.map((product) => (
+        <div key={product.handle} className="group relative">
+          <div className="h-96 w-full overflow-hidden rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-3 sm:h-auto">
+            <img
+              src={product.featuredImage?.url}
+              alt={product.featuredImage?.altText ?? ""}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <h3 className="mt-4 text-base font-semibold text-gray-900">
+            <Link to={`/products/${product.handle}`}>
+              <span className="absolute inset-0" />
+              {product.title}
+            </Link>
+          </h3>
+          {/* <p className="mt-1 text-sm text-gray-500">{product.price}</p> */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function Products() {
+  // https://github.com/remix-run/remix/issues/5211
+  // const { data_ } = useLoaderData<typeof loader>();
 
   return (
     <Container className="py-8">
       <h2 className="text-2xl font-bold tracking-tight text-gray-900">
         Products
       </h2>
-      <pre>{JSON.stringify(data_, null, 2)}</pre>
+      <ProductGrid />
+      {/* <pre>{JSON.stringify(data_, null, 2)}</pre> */}
     </Container>
   );
 }
