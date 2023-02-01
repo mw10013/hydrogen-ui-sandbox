c2ee68b26a1117dea57d0e36002a7aff64ec68a2
Michael Wu <mw10013@gmail.com>
2023-01-31 22:55:51 -0500

    wip

--- apps/storefront-kit-tailwindui-demo/app/routes/_layout.products.$handle.tsx
+++ apps/storefront-kit-tailwindui-demo/app/routes/_layout.products.$handle.tsx
@@ -1,8 +1,11 @@
+import { Container } from "@/components/Container";
 import { graphql } from "@/lib/gql";
+import { ProductQuery } from "@/lib/gql/graphql";
 import { shopClient } from "@/lib/utils";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import clsx from "clsx";
 import request from "graphql-request";
 import invariant from "tiny-invariant";
 
@@ -12,6 +15,13 @@ const query = graphql(`
       handle
       title
       description
+      images(first: 3) {
+        nodes {
+          id
+          url(transform: null)
+          altText
+        }
+      }
       options {
         name
         values
@@ -42,16 +52,63 @@ export const loader = (async ({ params }) => {
       handle: params.handle,
     },
   });
+  if (!data_.product) {
+    throw new Error(`Invalid product handle: ${params.handle}`);
+  }
   return json({
-    data_,
+    data_: { product: data_.product },
   });
 }) satisfies LoaderFunction;
+
+function ProductTitle() {
+  const { data_ } = useLoaderData<typeof loader>();
+  return (
+    <div className="flex justify-between">
+      <h1 className="text-xl font-medium text-gray-900">
+        {data_.product.title}
+      </h1>
+      <p className="text-xl font-medium text-gray-900">Price</p>
+    </div>
+  );
+}
+
+function ProductGallery() {
+  const { data_ } = useLoaderData<typeof loader>();
+  return (
+    <>
+      <h2 className="sr-only">Images</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-3 lg:gap-8">
+        {data_.product.images.nodes.map((image, index) => (
+          <img
+            key={image.id}
+            src={image.url}
+            alt={image.altText ?? ""}
+            className={clsx(
+              index === 0 ? "lg:col-span-2 lg:row-span-2" : "hidden lg:block",
+              "rounded-lg"
+            )}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
 
 export default function Product() {
   const { data_ } = useLoaderData<typeof loader>();
   return (
-    <div>
-      <pre>{JSON.stringify(data_, null, 2)}</pre>
-    </div>
+    <Container className="mt-8">
+      <div className="lg:grid lg:auto-rows-min lg:grid-cols-12 lg:gap-x-8">
+        <div className="lg:col-span-5 lg:col-start-8">
+          <ProductTitle />
+        </div>
+        <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
+          <ProductGallery />
+        </div>
+      </div>
+      <div>
+        <pre>{JSON.stringify(data_, null, 2)}</pre>
+      </div>
+    </Container>
   );
 }
