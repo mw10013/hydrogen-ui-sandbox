f29154c285dc3a198bfea69600a2f07e02904fba
Michael Wu <mw10013@gmail.com>
2023-02-05 17:13:19 -0500

    wip

--- apps/storefront-kit-tailwindui-demo/app/routes/tailwindui.shopping-cart-slide-over-components.tsx
+++ apps/storefront-kit-tailwindui-demo/app/routes/tailwindui.shopping-cart-slide-over-components.tsx
@@ -35,10 +35,12 @@ function ShoppingCart({
   open,
   setOpen,
   title,
+  children,
 }: {
   open: boolean;
   setOpen: (open: boolean) => void;
   title: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -89,47 +91,7 @@ function ShoppingCart({
                       <div className="mt-8">
                         <div className="flow-root">
                           <ul className="-my-6 divide-y divide-gray-200">
-                            {products.map((product) => (
-                              <li key={product.id} className="flex py-6">
-                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                  <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-full w-full object-cover object-center"
-                                  />
-                                </div>
-
-                                <div className="ml-4 flex flex-1 flex-col">
-                                  <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
-                                      <h3>
-                                        <a href={product.href}>
-                                          {product.name}
-                                        </a>
-                                      </h3>
-                                      <p className="ml-4">{product.price}</p>
-                                    </div>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {product.color}
-                                    </p>
-                                  </div>
-                                  <div className="flex flex-1 items-end justify-between text-sm">
-                                    <p className="text-gray-500">
-                                      Qty {product.quantity}
-                                    </p>
-
-                                    <div className="flex">
-                                      <button
-                                        type="button"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                                      >
-                                        Remove
-                                      </button>
-                                    </div>
-                                  </div>
-                                </div>
-                              </li>
-                            ))}
+                            {children}
                           </ul>
                         </div>
                       </div>
@@ -179,5 +141,43 @@ function ShoppingCart({
 export default function Example() {
   const [open, setOpen] = useState(true);
 
-  return <ShoppingCart title="Shopping cart" open={open} setOpen={setOpen} />;
+  return (
+    <ShoppingCart title="Shopping cart" open={open} setOpen={setOpen}>
+      {products.map((product) => (
+        <li key={product.id} className="flex py-6">
+          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+            <img
+              src={product.imageSrc}
+              alt={product.imageAlt}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+
+          <div className="ml-4 flex flex-1 flex-col">
+            <div>
+              <div className="flex justify-between text-base font-medium text-gray-900">
+                <h3>
+                  <a href={product.href}>{product.name}</a>
+                </h3>
+                <p className="ml-4">{product.price}</p>
+              </div>
+              <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+            </div>
+            <div className="flex flex-1 items-end justify-between text-sm">
+              <p className="text-gray-500">Qty {product.quantity}</p>
+
+              <div className="flex">
+                <button
+                  type="button"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ShoppingCart>
+  );
 }
